81ce5c216e216bf5e46cfac96851c4674bf5e03d
inti <intijuarez.prof@gmail>
2021-07-15 00:36:59 -0300

    segunda prueba

--- funcion.py
+++ funcion.py
@@ -1,6 +1,11 @@
-# ordenar por nombre y por edad.
+# ordenar por nombre y por edad, en lista de menor a mayor.
 # nombres: Inti, Lucas, Mike.
 #Edad: Inti: 25, Miki: 20, Lucas: 26
 
-name_list = (("inti: 24", "Mike: 10", "Lucas: 25", "Edu: 50", "Lauti: 22"))
-print(name_list)
+name_list = (("inti", "Mike", "Lucas", "Edu", "Lauti"))
+# edad = int[24, 10, 25, 50, 22]
+print(name_list[0])
+print(name_list[1])
+print(name_list[2])
+print(name_list[3])
+print(name_list[4])
